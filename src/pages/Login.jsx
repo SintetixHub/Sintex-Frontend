977456b41f03de30ff304861fb224be371af73d4
Ebanx3 <esteban2santos@gmail.com>
2024-01-30 23:21:38 -0300

    little correction

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-[15vh]">
       <button
-        className="mb-5 border border-blue-600 dark:border-orange-600 text-black dark:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 hover:scale-105"
+        className="mb-5 border border-blue-600 dark:border-orange-600 text-black dark:text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 hover:scale-105 w-72"
         onClick={handleSwitch}
       >
         {isLogin
