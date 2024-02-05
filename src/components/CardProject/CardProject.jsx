59f6e8afbff8f200b13ffecd5f63c03ba2fcd273
Ebanx3 <esteban2santos@gmail.com>
2024-02-05 00:29:07 -0300

    implemented route projects

--- src/components/CardProject/CardProject.jsx
+++ src/components/CardProject/CardProject.jsx
@@ -1,24 +1,27 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
+// eslint-disable-next-line react/prop-types
 const CardProject = ({ project }) => {
-    return (
-        <Link to={`/project/${project._id}`} className="flex rounded-xl py-4 px-8 bg-zinc-300 text-black dark:text-white dark:bg-zinc-800">
-            <div className="flex flex-col gap-4 items-center">
-                <img
-                    src={project.AuthorAvatar}
-                    alt={`${project.authorUsername} avatar`}
-                    className="h-44 w-44 rounded-full object-cover border-2 border-black dark:border-zinc-400"
-                />
-                <span>{project.authorUsername}</span>
-            </div>
-            <div className="p-4 flex flex-col gap-4 flex-1">
-                <div className="flex justify-between">
-                    <h2 className="text-xl font-bold">{project.title}</h2>
-                </div>
-                <p>{project.content}</p>
-            </div>
-        </Link>
-    );
+  console.log(project);
+  return (
+    <Link
+      to={`/project/${project.title}`}
+      className="flex flex-col  rounded-xl p-8 bg-zinc-200 text-black dark:text-white dark:bg-zinc-800 hover:bg-zinc-300 dark:hover:bg-zinc-700"
+    >
+      <h2 className="text-2xl font-bold">{project.title}</h2>
+      <span className="text-zinc-700 dark:text-zinc-300 font-medium text-lg">
+        {project.authorName}
+      </span>
+
+      <p className="text-zinc-600 dark:text-zinc-400 my-4">
+        {project.description}
+      </p>
+      <span className="self-end font-medium text-zinc-600 dark:text-zinc-400">
+        Miembros: {project.membersList.length}/{project.membersCount}
+      </span>
+    </Link>
+  );
 };
 
 export default CardProject;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import CardProject from '../components/CardProject/CardProject';
-import { getAllProjects } from '../api/getProjects';
-
+import { useState, useEffect } from "react";
+import CardProject from "../components/CardProject/CardProject";
+import { getAllProjects } from "../api/getProjects";
+import SearchBar from "../components/SearchBar/SearchBar";
 export default function Projects() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     (async () => {
       const response = await getAllProjects();
+      console.log(response);
       if (response && response.success) {
         setProjects(response.data);
       }
@@ -15,10 +16,13 @@ export default function Projects() {
   }, []);
 
   return (
-    <div className="space-y-4 ">
-      {projects.map((project) => (
-        <CardProject key={project._id} project={project} />
-      ))}
-    </div>
+    <>
+      <SearchBar />
+      <div className="grid grid-cols-3 mt-6">
+        {projects.map((project) => (
+          <CardProject key={project._id} project={project} />
+        ))}
+      </div>
+    </>
   );
 }
