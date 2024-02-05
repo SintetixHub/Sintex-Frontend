1cf8e1adaebc00cd6c9a19f68db37c87a95dd294
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2024-02-04 23:38:15 -0500

    Update menu styles

--- src/components/Navbar/UserButton.jsx
+++ src/components/Navbar/UserButton.jsx
@@ -1,17 +1,37 @@
-/* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import UserMenu from "./UserMenu";
 
-// eslint-disable-next-line react/prop-types
 export default function UserButton({ username }) {
   const [menuVisible, setMenuVisible] = useState(false);
+  const node = useRef();
 
   const handleClick = () => {
     setMenuVisible(!menuVisible);
   };
 
+  const handleClickOutside = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click 
+    setMenuVisible(false);
+  };
+
+  useEffect(() => {
+    if (menuVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuVisible]);
+
   return (
-    <>
+    <div ref={node}>
       <button
         className="font-bold py-1 px-4  rounded-md  text-sky-500 hover:text-sky-300  dark:hover:text-orange-300 dark:text-orange-400 "
         onClick={handleClick}
@@ -19,6 +39,6 @@ export default function UserButton({ username }) {
         {username}
       </button>
       {menuVisible && <UserMenu username={username} />}
-    </>
+    </div>
   );
 }
--- src/components/Navbar/UserMenu.jsx
+++ src/components/Navbar/UserMenu.jsx
@@ -1,22 +1,24 @@
-/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 
 export default function UserMenu({ username }) {
   return (
-    <div className="absolute top-16 right-20 border-2 border-cyan-400 dark:border-orange-400 p-4 rounded-md gap-2 flex flex-col items-stretch">
-      <Link to={`/user/${username}`} className="hover:text-zinc-400">
+    <div className="absolute top-16 right-20 border-2   dark:bg-red-100 opacity-90 bg-blue-100 border-cyan-500 dark:border-orange-500 p-4 rounded-md gap-2 flex flex-col items-stretch shadow-lg">
+      <Link to={`/user/${username}`} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition-all transform duration-300 ease-out hover:scale-110 text-center  ">
         Perfil
       </Link>
-      <Link to="/project" className="hover:text-zinc-400">
-        Project
+      <Link 
+        to="/project" 
+        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition-all transform duration-300 ease-out hover:scale-110 text-center"
+      >
+        Proyecto
       </Link>
-      <Link to="/newProject" className="hover:text-zinc-400">
+      <Link to="/newProject" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  transform ease-out hover:scale-110 transition-colors duration-300 text-center">
         Nuevo Proyecto
       </Link>
-      <Link to="/newPost" className="hover:text-zinc-400">
+      <Link to="/newPost" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transform ease-out hover:scale-110 transition-colors duration-300 text-center">
         Nuevo Post
       </Link>
-      <button className="hover:text-zinc-400 w-8">
+      <button className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transform ease-out hover:scale-110 transition-colors duration-300 text-center">
         <span>Salir</span>
       </button>
     </div>
